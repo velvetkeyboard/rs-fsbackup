484a6c03e86f8896b11e7e99c393e75675adf5f5
Ramon Moraes <ramon@vyscond.io>
2020-03-23 15:35:05 -0300

    Backup

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,36 @@ extern crate rusoto_s3;
 extern crate shellexpand;
 extern crate walkdir;
 
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
+// use walkdir::DirEntry;
 // use std::path::Path;
 use std::fs::read_to_string;
 use yaml_rust::YamlLoader;
-use rusoto_core::region::Region;
-use rusoto_s3::S3Client;
+// use rusoto_core::region::Region;
+use rusoto_credential::ProfileProvider;
+// use rusoto_s3::S3Client;
+use std::env;
 
 fn main() {
-    let contents = read_to_string("config.yaml").expect("Unable to read file");
+    let args: Vec<String> = env::args().collect();
+    // let config_path = ;
+    // println!("Reading config from: {}", config_path);
+    let contents = read_to_string(args[1].clone()).expect("Unable to read file");
     let config = YamlLoader::load_from_str(&contents).unwrap();
     let backups = &config[0]["backups"].as_hash().unwrap();    
 
     for device_name in backups.keys() {
         println!("Backing up for device {:?}", device_name.as_str().unwrap());
+        // println!("{:?}", backups[device_name].as_vec().unwrap());
         for entry in backups[device_name].as_vec().unwrap().iter() {
-            println!("  Processing {:?}", entry["name"].as_str().unwrap());
-            let source = &entry["source"];
+            println!("\tProcessing {:?}", entry.as_str().unwrap());
+            // let source = &entry["source"];
+            let file_path = entry.as_str().unwrap();
 
             // Check if it's a file or not
             let mut expanded_source = String::from("");
             expanded_source.push_str(
-                &shellexpand::tilde(source.as_str().unwrap()).to_string()
+                &shellexpand::tilde(file_path).to_string()
             );
 
             // if Path::new(&expanded_source).is_file() {
@@ -36,13 +44,23 @@ fn main() {
             // }
 
             for entry in WalkDir::new(&expanded_source).into_iter() {
-                let full_file_path = entry.ok().unwrap().into_path().to_str().unwrap().to_owned();
-                let mut s3_key = String::from("");
-                s3_key.push_str(device_name.as_str().unwrap());
-                s3_key.push_str("/");
-                s3_key.push_str(&full_file_path);
-                println!("    Uploading {} to s3://{}", source.as_str().unwrap(), s3_key);
-                let mut client = S3Client::new(Region::UsEast1);
+                let option_full_file_path = entry.ok();
+                if !option_full_file_path.is_none() {
+                    let full_file_path = option_full_file_path.unwrap().into_path().to_str().unwrap().to_owned();
+                    println!("{:?}", full_file_path);
+                    let mut s3_key = String::from("");
+                    s3_key.push_str(device_name.as_str().unwrap());
+                    s3_key.push_str("/");
+                    s3_key.push_str(&full_file_path);
+                    println!("\t\tUploading {} to s3://{}", file_path, s3_key);
+                    let pp = ProfileProvider::new();
+                    pp.set_profile("default")
+                    // let mut client = S3Client::new(Region::UsEast1);
+                    let mut client = S3Client::new_with()
+                }
+                // let full_file_path = entry.ok().unwrap();//.into_path().to_str().unwrap().to_owned();
+                // println!("{:}?", full_file_path);
+                // println!("{:?}", full_file_path);
             }
         }
     }
